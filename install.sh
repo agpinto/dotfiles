95f8727ff9f0b0cfeb7d5e3ecdfadcd086062ca8
agpinto <agustinxpinto@hotmail.com>
2018-03-14 21:52:53 -0300

    added Visual Studio Code

--- install.sh
+++ install.sh
@@ -17,13 +17,32 @@ function link_file_to_home {
 
 
 # List of Applications to be installed
-PACKAGES='git zsh screen vim-nox python-pip tree'
+PACKAGES='git zsh screen python-pip tree'
 
 # Install applications listed above
 if [ -f /etc/redhat-release ]; then 
     sudo yum update -y && sudo yum install -y $PACKAGES
+    
+    if [! -f $(which vim) ]; then
+        sudo yum install -y vim
+    fi
+
+    # Visual Studio Code
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    yum check-update
+    sudo yum install code
+
 elif [ -f /etc/lsb-release ]; then 
-    sudo apt-get update && sudo apt-get install -y $PACKAGES
+    sudo apt-get update && sudo apt-get install -y $PACKAGES && sudo apt-get install -y vim-nox
+
+    # Visual Studio Code
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo apt-get update
+    sudo apt-get install code # or code-insiders
+
 else
     echo "ERROR - Release file not found"
     echo "ERROR - Can not determine package management"
